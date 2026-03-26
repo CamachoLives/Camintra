aeb39a00432c496ba237203def159cfe08a8d7ee
Christian Alexander Camacho <rfcamacris@gmail.com>
2026-03-25 22:42:15 -0500

    w3

--- src/app/services/configuracion/parametrizacion.service.ts
+++ src/app/services/configuracion/parametrizacion.service.ts
@@ -23,6 +23,7 @@ export class ConfiguracionService {
     }).pipe(catchError(this.handleError));
   }
 
+  
   private handleError(error: HttpErrorResponse): Observable<never> {
     let errorMessage = 'Ha ocurrido un error inesperado';
 
