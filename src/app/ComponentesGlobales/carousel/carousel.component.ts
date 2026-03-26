e1150f0e1c4c476685ad150ecc809471b6ee84ed
Christian Alexander Camacho <rfcamacris@gmail.com>
2026-03-25 21:58:22 -0500

    Integracion de inicio con nombre

--- src/app/ComponentesGlobales/carousel/carousel.component.ts
+++ src/app/ComponentesGlobales/carousel/carousel.component.ts
@@ -8,9 +8,9 @@ import { CommonModule } from '@angular/common';
 })
 export class CarouselComponent {
   images = [
-    '/assets/1.jpg',
+    '/assets/3.jpg',
     '/assets/2.jpg',
-    '/assets/3.jpg'
+    '/assets/1.jpg'
   ];
   currentIndex = 0;
 
--- src/app/Inicio/interfaz/inicio.component.ts
+++ src/app/Inicio/interfaz/inicio.component.ts
@@ -16,67 +16,10 @@ export class inicioComponent  {
   user: any;
   error: string = '';
   TituloEntrada = '';
-  clientes = [
-    {
-      name: 'Brinsa',
-      level: 'High',
-      activity: 'development',
-      porcentage: '60',
-    },
-    {
-      name: 'Mnemo',
-      level: 'Medium',
-      activity: 'Cibersecurity',
-      porcentage: '80',
-    },
-    {
-      name: 'Siscomputo',
-      level: 'Low',
-      activity: 'Infraestructure',
-      porcentage: '20',
-    },
-  ];
-
-  contacts = [
-    {
-      name: 'Cristian Camacho',
-      phone: '3148917721',
-      image: 'https://github.com/CamachoLives.png',
-      contacts: 87,
-      gradientColor: 'bg-gradient-to-tr from-blue-600 to-blue-400',
-      contactColor: 'text-green-500',
-    },
-    {
-      name: 'Esteban Heredia',
-      phone: '3202152786',
-      image: 'https://github.com/here0503.png',
-      contacts: 43,
-      gradientColor: 'bg-gradient-to-tr from-green-600 to-green-400',
-      contactColor: 'text-yellow-500',
-    },
-    {
-      name: 'Luis Villamizar',
-      phone: '3118145001',
-      image: 'https://github.com/LuisVillamizar.png',
-      contacts: 15,
-      gradientColor: 'bg-gradient-to-tr from-red-600 to-red-400',
-      contactColor: 'text-red-500',
-    },
-  ];
+  username: string = '';
 
 
-
-  // constructor(private userService: UserService) {}
-  // ngOnInit() {
-  //   this.userService.getInformation().subscribe({
-  //     next: (res) => {
-  //       this.user = res.body.nombre;
-  //       this.correo = res.body.email;
-  //     },
-  //     error: (err) => {
-  //       this.error = 'Incorrect email';
-  //       console.error(err);
-  //     },
-  //   });
-  // }
+  constructor(private userService: UserService) {}
+  userName: string = 'Cristian Alexander Camacho';
+ 
 }
